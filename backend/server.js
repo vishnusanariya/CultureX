ddcfba23953b3b10f3af2db61ca941bc777dcef8
Vishnu Sanariya <vishnusanariya98@gmail.com>
2023-07-24 14:24:33 +0530

    url update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://culture-x-client.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
